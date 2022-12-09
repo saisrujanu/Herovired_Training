0ee35e0335138121713f9654e06c07347a4acf82
Sai Srujan <70358813+saisrujanu@users.noreply.github.com>
2022-12-09 10:23:20 +0530

    second programs

---
+++ 05-12-2022.py
@@ -0,0 +1,35 @@
+# a=0
+# b=0
+# n=str(input("Enter the Vehicle Name: "))
+# num=int(input("Enter the number of Vehicles:"))
+#  if n == 'car':
+#    b=40
+#  elif n == 'bike':
+#    a=20  
+#  else:
+#    print (0)
+# total=a+b
+# print(total)
+#ParkingLot
+#Camel Casing
+# noOfCars=0
+# noOfBikes=0
+# totalPayment=0
+# noOfCars=int(input("No Of Cars:"))
+# noOfBikes=int(input("No Of Bikes:"))
+# totalPayment=noOfBikes*20+noOfCars*40
+# print("Total Payment to be pay:",totalPayment)
+
+
+# noofsanjoy="Sanjay"
+# #type independent
+# print(noofsanjoy)
+
+product1=int(input("Enter the price of Product1:"))
+product2=int(input("Enter the price of Product2:"))
+product3=int(input("Enter the price of Product3:"))
+Quantity1=int(input("Enter the Quantity1"))
+Quantity2=int(input("Enter the Quantity2"))
+Quantity3=int(input("Enter the Quantity3"))
+totalamount=product1*Quantity1+product2*Quantity2+product3*Quantity3
+print("Total Amount to be paid by user:",totalamount)
